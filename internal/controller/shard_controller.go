4122ea25fb33e2a3b571baed2d4eff2d2b9d0d72
Anubhav <anubhavaeron@gmail.com>
2023-08-14 14:51:48 -0700

    use expected spec crd for assetdb and shard

--- internal/controller/shard_controller.go
+++ internal/controller/shard_controller.go
@@ -33,9 +33,9 @@ type ShardReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=admiral.io.admiral.io,resources=shards,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=admiral.io.admiral.io,resources=shards/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=admiral.io.admiral.io,resources=shards/finalizers,verbs=update
+//+kubebuilder:rbac:groups=admiral.io,resources=shards,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=admiral.io,resources=shards/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=admiral.io,resources=shards/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
